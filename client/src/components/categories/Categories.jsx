159ff431cdd5cd9a79aea95f192803f20c4a45da
Umit Turk <uyt.turk@gmail.com>
2023-02-18 16:55:49 +0300

    Categories component created

---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,41 @@
+const Categories = () => {
+  return (
+    <ul className="flex gap-4 flex-col text-lg">
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>All</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Food</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+        <span>Beverage</span>
+      </li>
+    </ul>
+  );
+};
+
+export default Categories;
